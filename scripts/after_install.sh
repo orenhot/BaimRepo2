9eb697efba8d193b033b551160e743af3dfaf272
arief <ariefrmail@gmail.com>
2022-04-24 16:49:09 +0700

    push index.php

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,4 +2,4 @@
 EC2_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 sed -i "s/Delivery/Delivery on $EC2_INSTANCE_ID in $EC2_AZ/g" /var/www/html/index.php
-chmod 664 /var/www/html/index.php
+chmod -R 777 /var/www/html/index.php
