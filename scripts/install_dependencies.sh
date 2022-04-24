4e60263bb157c529d47a01120365f365ef654a95
arief <ariefrmail@gmail.com>
2022-04-24 16:20:05 +0700

    install_depend

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 yum install -y httpd
-yum install -y php72
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+wget http://rpms.remirepo.net/enterprise/remi-release-6.rpm
+rpm -Uvh remi-release-6.rpm
+rpm -Uvh epel-release-latest-6.noarch.rpm
+yum install yum-utils
+yum-config-manager --enable remi-php72
+yum install --disablerepo="*" --enablerepo="remi,remi-php72" php php-fpm php-cl
+yum install php72
 
